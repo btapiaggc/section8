a42775b5e1faeb5b8400fdd4e7647c675dc53efa
btapiaggc <48529515+btapiaggc@users.noreply.github.com>
2019-03-13 15:29:47 -0400

    Update SumSquare.java

--- SumSquare.java
+++ SumSquare.java
@@ -1,2 +1,24 @@
-public class SumSquare {
-public static void main(String[] args) {
+package gitHubSection_8;
+public class WhileSumSquare {
+	public static void main(String[] args) {
+	
+		int count= 0;
+		int result = 0;
+		while (count < 33);
+		{
+			count += 3;
+			
+			result = count * count;
+			
+			
+			
+		System.out.println("Cumulation result: " + result + " : " + count + " * "  + count);
+			
+		}		
+		
+	
+	
+	
+
+}
+	}
